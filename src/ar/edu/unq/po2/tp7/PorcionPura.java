5cdd1315c9851824de986f1c0cc86ed6bf132120
nicom <nicom@10.12.4.89>
2025-10-02 17:50:59 -0300

    Empiezo TP Composite

---
+++ src/ar/edu/unq/po2/tp7/PorcionPura.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.po2.tp7;
+
+public class PorcionPura extends PorcionesDeTierra{
+	public Cultivo plantacion;
+	
+	public PorcionPura(Cultivo plantacion) {
+		this.plantacion = plantacion;
+	}
+}
---
+++ src/ar/edu/unq/po2/tp7/Soja.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.po2.tp7;
+
+public class Soja extends Cultivo{
+	public double precio;
+	
+	public Soja(double precio) {
+		this.precio = precio;
+	}
+	
+	public double getPrecio() {
+		return this.precio;
+	}
+}
